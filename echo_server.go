febe2026e9af0c3fe8e73b82755e6fe5bb59a0c2
Derek Parker <parkerderek86@gmail.com>
2013-09-09 08:30:01 -0500

    Add function to remove connection from list

--- echo_server.go
+++ echo_server.go
@@ -6,6 +6,14 @@ import (
 	"net"
 )
 
+func removeConnection(connections *list.List, connection net.Conn) {
+	for c := (*connections).Front(); c != nil; c = c.Next() {
+		if c.Value.(net.Conn) == connection {
+			(*connections).Remove(c)
+		}
+	}
+}
+
 func handleConnection(connection net.Conn, connections *list.List) {
 	var conn net.Conn
 	msg := make([]byte, 1024)
@@ -13,6 +21,8 @@ func handleConnection(connection net.Conn, connections *list.List) {
 	for {
 		if _, err := connection.Read(msg); err != nil {
 			log.Printf("Connection closed: %s", connection.RemoteAddr().String())
+			connection.Close()
+			removeConnection(connections, connection)
 			break
 		}
 		log.Printf("Message received: %s", string(msg))
